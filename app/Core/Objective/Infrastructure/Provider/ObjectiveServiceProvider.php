94a34c985b9e0e0358afd92e8e0103a65321746e
Geekers-Joel237 <79468070+Geekers-Joel237@users.noreply.github.com>
2024-11-26 05:15:06 +0100

    [FEATURE] objectives: list and filter objectives, migrations ok

--- app/Core/Objective/Infrastructure/Provider/ObjectiveServiceProvider.php
+++ app/Core/Objective/Infrastructure/Provider/ObjectiveServiceProvider.php
@@ -6,5 +6,14 @@ use Illuminate\Support\ServiceProvider;
 
 class ObjectiveServiceProvider extends ServiceProvider
 {
-    public function register(): void {}
+    public function register(): void
+    {
+        $this->loadMigrations();
+    }
+
+    private function loadMigrations(): void
+    {
+        $this->loadMigrationsFrom(base_path('app/Core/Objective/Infrastructure/database/migrations'));
+
+    }
 }
