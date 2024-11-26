6b76f9780084e54f0e1492780993e4e3716f193a
Geekers-Joel237 <79468070+Geekers-Joel237@users.noreply.github.com>
2024-11-26 05:12:20 +0100

    [FEATURE] objectives: list and filter objectives, migrations ok

---
+++ app/Core/Objective/Infrastructure/database/migrations/2024_11_26_040753_create_objectives_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('objectives', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('project_id')->index('project_id');
+            $table->string('owner_id')->index('owner_id');
+            $table->json('tasks');
+            $table->softDeletes();
+            $table->boolean('is_deleted')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('objectives');
+    }
+};
