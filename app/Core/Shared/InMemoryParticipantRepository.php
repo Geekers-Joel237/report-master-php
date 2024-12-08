846305581e6aafd6706be22afbe85afcc2a1a5d6
Geekers-Joel237 <79468070+Geekers-Joel237@users.noreply.github.com>
2024-12-08 17:37:38 +0100

    [FEATURE] user: create and update user

--- app/Core/Shared/InMemoryParticipantRepository.php
+++ app/Core/Shared/InMemoryParticipantRepository.php
@@ -2,6 +2,8 @@
 
 namespace App\Core\Shared;
 
+use App\Core\User\Domain\Entities\User;
+use App\Core\User\Domain\Snapshot\UserSnapshot;
 use App\Core\User\Domain\WriteUserRepository;
 
 class InMemoryParticipantRepository implements WriteUserRepository
@@ -13,5 +15,25 @@ class InMemoryParticipantRepository implements WriteUserRepository
     public function allExists(array $participantIds): array
     {
         return array_intersect($participantIds, $this->participantIds);
+    }
+
+    public function emailExists(string $email, ?string $userId): bool
+    {
+        // TODO: Implement emailExists() method.
+    }
+
+    public function save(UserSnapshot $user): void
+    {
+        // TODO: Implement save() method.
+    }
+
+    public function ofId(?string $userId): ?User
+    {
+        // TODO: Implement ofId() method.
+    }
+
+    public function update(UserSnapshot $snapshot): void
+    {
+        // TODO: Implement update() method.
     }
 }
