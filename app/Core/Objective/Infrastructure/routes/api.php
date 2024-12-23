aa3acbef0d8c9517b9dd366f0392c82b76b38d1f
Geekers-Joel237 <79468070+Geekers-Joel237@users.noreply.github.com>
2024-12-23 11:40:19 +0100

    merge from develop

--- app/Core/Objective/Infrastructure/routes/api.php
+++ app/Core/Objective/Infrastructure/routes/api.php
@@ -5,6 +5,8 @@ use App\Core\Objective\Infrastructure\Http\Controllers\GetAllObjectivesAction;
 use App\Core\Objective\Infrastructure\Http\Controllers\SaveObjectiveAction;
 use Illuminate\Support\Facades\Route;
 
-Route::post('objectives', SaveObjectiveAction::class);
-Route::delete('objectives/{objectiveId}', DeleteObjectiveAction::class);
-Route::get('objectives', GetAllObjectivesAction::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('objectives', SaveObjectiveAction::class);
+    Route::delete('objectives/{objectiveId}', DeleteObjectiveAction::class);
+    Route::get('objectives', GetAllObjectivesAction::class);
+});
--- app/Core/Project/Infrastructure/routes/api.php
+++ app/Core/Project/Infrastructure/routes/api.php
@@ -5,6 +5,8 @@ use App\Core\Project\Infrastructure\Http\Controllers\GetAllProjectsAction;
 use App\Core\Project\Infrastructure\Http\Controllers\SaveProjectAction;
 use Illuminate\Support\Facades\Route;
 
-Route::get('projects', GetAllProjectsAction::class);
-Route::post('projects', SaveProjectAction::class);
-Route::delete('projects/{projectId}', DeleteProjectAction::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('projects', GetAllProjectsAction::class);
+    Route::post('projects', SaveProjectAction::class);
+    Route::delete('projects/{projectId}', DeleteProjectAction::class);
+});
